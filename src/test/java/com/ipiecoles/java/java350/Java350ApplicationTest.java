069e3530bb26f68e0879e1b40eb0cfbea3ec82d0
gbresolin <gregory.bresolin@ipilyon.net>
2020-04-08 15:16:36 +0200

    Test Java350Application

---
+++ src/test/java/com/ipiecoles/java/java350/Java350ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.ipiecoles.java.java350;
+
+import org.junit.jupiter.api.Test;
+
+public class Java350ApplicationTest {
+    @Test
+    public void testmain(){
+        Java350Application.main(new String[]{
+
+        });
+    }
+}
